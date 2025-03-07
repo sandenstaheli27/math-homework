aa1a9e99bef2032c0c814c56a2b50545da581380
sandenstaheli27 <sandenstaheli27@gmail.com>
2025-03-07 06:20:35 +0000

    Add 2.c

---
+++ src/2.c
@@ -0,0 +1,16 @@
+
+int main() {
+    int a = 5;
+    double b = 3.14;
+    char c = 'c';
+    bool d = true;
+    string e = "string";
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+
+    return 0;
+}
