1a8a100e12cc22beef9673798b78396017702c87
sandenstaheli27 <sandenstaheli27@gmail.com>
2025-03-09 08:56:51 +0000

    Add 4.c

---
+++ src/4.c
@@ -0,0 +1,9 @@
+int main() {
+    // Generate a random number between 1 and 10
+    int num = rand() % 10 + 1;
+
+    // Print the number to the console
+    printf("Your random number is: %d\n", num);
+
+    return 0;
+}
