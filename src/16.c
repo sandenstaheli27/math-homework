1c43b97da0ecfb9900b47c50273d07c08fa783bc
sandenstaheli27 <sandenstaheli27@gmail.com>
2025-03-23 01:49:39 +0000

    Add 16.c

---
+++ src/16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        printf("The number is positive.");
+    } else if (x == 0) {
+        printf("The number is zero.");
+    } else {
+        printf("The number is negative.");
+    }
+    return 0;
+}
