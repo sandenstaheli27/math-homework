d431c1215206e1a98c8e8348626ac27d84fc44b6
sandenstaheli27 <sandenstaheli27@gmail.com>
2025-04-07 03:12:00 +0000

    Add 25.c

---
+++ src/25.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main() {
+    int a = 5;
+    int b = 10;
+    float c = 3.14;
+
+    printf("The value of a + b is: %d\n", a + b);
+    printf("The value of c is: %.2f\n", c);
+}
